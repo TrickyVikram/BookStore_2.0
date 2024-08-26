2d0af59a404502748c6bda8cd26f8e531a2b670a
TrickyVikram <vkumarsah999@gmail.com>
2024-08-26 17:32:31 +0530

    update12

--- backend/server.js
+++ backend/server.js
@@ -12,28 +12,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected')).catch((err) => console.error(err));
-
-// app.use('/api/users', authRoutes);
-// app.use('/api/books', bookRoutes);
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.error(err));
 
 app.use('/api/books', (req, res, next) => {
-    console.log("Books API hit"); // Log request to ensure route is hit
+    console.log("Books API hit");
     next();
 }, bookRoutes);
 
 app.use('/api/users', (req, res, next) => {
-    console.log("Users API hit"); // Log when the route is accessed
+    console.log("Users API hit ");
     next();
 }, authRoutes);
 
-
 app.get('/', (req, res) => {
     res.json({ message: 'Api work' });
-}); // Added closing parenthesis here
+});
 
 const PORT = process.env.PORT || 4500;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
